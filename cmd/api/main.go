8c7026dd2dd3e1cf8cd298644e5a49e34f27d0cc
devkcud <patommmmm@proton.me>
2024-04-06 20:35:35 -0300

    feat: Add login and register

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 
+	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/controller"
+	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/loader"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	loader.LoadEnv()
+	loader.LoadDB()
+
 	gin.SetMode(gin.ReleaseMode)
 
 	port := os.Getenv("PORT")
@@ -23,15 +27,12 @@ func createRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 
+	// XXX: API v1
 	v1 := router.Group("/v1")
+	user := v1.Group("/user")
 	{
-		v1.GET("/hello", func(ctx *gin.Context) {
-			ctx.String(http.StatusOK, "Hello, world!")
-		})
-
-		v1.GET("/hello/:name", func(ctx *gin.Context) {
-			ctx.String(http.StatusOK, fmt.Sprintf("Hello, %s!", ctx.Param("name")))
-		})
+		user.POST("/register", controller.Register)
+		user.POST("/login", controller.Login)
 	}
 
 	return router
---
+++ internal/loader/env.go
@@ -0,0 +1,15 @@
+package loader
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+}

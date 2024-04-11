e69c51137e7d9545f015e64b201ad7c82a1b4625
devkcud <patommmmm@proton.me>
2024-04-11 18:33:46 -0300

    fix: Auth handlers working and returning proper status codes

--- internal/controller/auth/handlers.go
+++ internal/controller/auth/handlers.go
@@ -4,32 +4,67 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/model"
 	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/utils"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func RegisterHandler(ctx *gin.Context) {
-	var body struct {
-		Fullname string `json:"fullname"`
-		Username string `json:"username"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+	var body UserBodyRegister
 
+	// Attempt to bind the request body to the UserBodyRegister struct
 	if err := ctx.BindJSON(&body); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad body formatting"})
 		return
 	}
 
-	userID, err := register(body.Fullname, body.Username, body.Email, body.Password)
+	var exists bool
+	// Check if user already exists by username or email
+	if err := utils.DB.Model(&model.User{}).Select("count(*) > 0").Where("username = ? OR email = ?", body.Username, body.Email).Find(&exists).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Something wrong happened in our servers. Try again later."})
+		return
+	}
+
+	// Check if the user already exists
+	if exists {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+		return
+	}
+
+	// Hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
 
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		// Check for a specific bcrypt error
+		if err == bcrypt.ErrPasswordTooLong {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "The password is too long"})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Something wrong happened in our servers. Try again later."})
+		}
+
 		log.Println(err)
 		return
 	}
 
-	token, err := utils.GenerateJWT(userID)
+	// Create a new User model with the provided and hashed information
+	user := model.User{
+		Fullname: body.Fullname,
+		Username: body.Username,
+		Email:    body.Email,
+		Password: string(hashedPassword),
+	}
+
+	// Attempt to create the user record in the database
+	if err := utils.DB.Create(&user).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Something wrong happened in our servers. Try again later."})
+		log.Println(err)
+		return
+	}
+
+	// Attempt to generate a JWT token for the new user
+	token, err := utils.GenerateJWT(user.ID)
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
@@ -41,32 +76,55 @@ func RegisterHandler(ctx *gin.Context) {
 }
 
 func LoginHandler(ctx *gin.Context) {
-	var body struct {
-		Username string `json:"username" default:""`
-		Email    string `json:"email" default:""`
-		Password string `json:"password" default:""`
-	}
+	var body UserBodyLogin
 
+	// Attempt to bind the incoming JSON to the struct
 	if err := ctx.ShouldBindJSON(&body); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad body formatting"})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad JSON format."})
 		return
 	}
 
-	userID, err := login(body.Username, body.Email, body.Password)
+	// Ensure either username or email is provided for authentication
+	if body.Username == "" && body.Email == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Either username or email must be provided."})
+		return
+	}
 
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	var user model.User
+
+	// Check if a user exists with the given username or email
+	if err := utils.DB.Where("username = ? OR email = ?", body.Username, body.Email).First(&user).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials. Please check your email/username and password."})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Something wrong happened in our servers. Try again later."})
+		}
+
 		log.Println(err)
 		return
 	}
 
-	token, err := utils.GenerateJWT(userID)
+	// Authenticate the user by comparing the hashed password
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password)); err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials. Please check your email/username and password."})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Something wrong happened in our servers. Try again later."})
+		}
 
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
 		log.Println(err)
 		return
 	}
 
+	// Generate JWT token for authenticated user
+	token, err := utils.GenerateJWT(user.ID)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Something wrong happened in our servers. Try again later."})
+		log.Println(err)
+		return
+	}
+
+	// Return the generated token to the user
 	ctx.JSON(http.StatusOK, gin.H{"token": token})
 }
--- internal/controller/auth/types.go
+++ internal/controller/auth/types.go
@@ -1,16 +1,5 @@
 package auth
 
-import (
-	"errors"
-
-	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/model"
-	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/utils"
-	"golang.org/x/crypto/bcrypt"
-)
-
-var ErrUserAlreadyDefined error = errors.New("user already exists with that name or email")
-var ErrInvalidUsernameOrEmail error = errors.New("not a valid username or email")
-
 type UserBodyRegister struct {
 	Fullname string `json:"fullname"`
 	Username string `json:"username"`
@@ -19,55 +8,7 @@ type UserBodyRegister struct {
 }
 
 type UserBodyLogin struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func register(fullname, username, email, password string) (uint, error) {
-	var exists bool
-	if err := utils.DB.Model(&model.User{}).Select("count(*) > 0").Where("username = ? OR email = ?", username, email).Find(&exists).Error; err != nil || exists {
-		return 0, err
-	}
-
-	if exists {
-		return 0, ErrUserAlreadyDefined
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-
-	if err != nil {
-		return 0, nil
-	}
-
-	user := model.User{
-		Fullname: fullname,
-		Username: username,
-		Email:    email,
-		Password: string(hashedPassword),
-	}
-
-	if err := utils.DB.Create(&user).Error; err != nil {
-		return 0, err
-	}
-
-	return user.ID, nil
-}
-
-func login(username, email, password string) (uint, error) {
-	if username == "" && email == "" {
-		return 0, ErrInvalidUsernameOrEmail
-	}
-
-	var user model.User
-
-	if err := utils.DB.Model(&model.User{}).Where("username = ? OR email = ?", username, email).First(&user).Error; err != nil {
-		return 0, err
-	}
-
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-		return 0, err
-	}
-
-	return user.ID, nil
+	Username string `json:"username" default:""`
+	Email    string `json:"email" default:""`
+	Password string `json:"password" default:""`
 }

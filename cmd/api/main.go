0598b32e5b5a9b5190480c453c890602677d21f1
devkcud <patommmmm@proton.me>
2024-04-19 19:56:56 -0300

    chore: Add config files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,43 +4,24 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/controller/auth"
-	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/model"
-	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/utils"
+	"github.com/devkcud/arkhon-foundation/arkhon-api/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Load funcs cannot run in goroutines due to dependency of the program on their global variables
-	// e.g.: utils.DB and env variables
-	utils.LoadEnv()
-	utils.LoadDB()
+	config.Parse()
 
-	// Using a goroutine for AutoMigrate prevents thread blocking,
-	// allowing the rest of the application to run smoothly.
-	go utils.DB.AutoMigrate(&model.User{})
+	gin.SetMode(config.Router.GinMode)
 
-	gin.SetMode(gin.ReleaseMode)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-
-	createRouter().Run(fmt.Sprintf(":%s", port))
-}
-
-func createRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 
-	// XXX: API v1
-	v1 := router.Group("/v1")
-	user := v1.Group("/user")
-	{
-		user.POST("/register", auth.RegisterHandler)
-		user.POST("/login", auth.LoginHandler)
+	// NOTE: Prioritize the PORT env variable, as some web services may set it
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = fmt.Sprint(config.Router.Port)
 	}
 
-	return router
+	router.Run(fmt.Sprintf("%s:%s", config.Router.Address, port))
 }

88c48a732dc476896f5332dc657549d88a202a4a
devkcud <patommmmm@proton.me>
2024-04-24 22:49:34 -0300

    refactor: Spacing out the validations in fields

--- internal/model/user.go
+++ internal/model/user.go
@@ -16,16 +16,16 @@ type User struct {
 	FirstName string `validate:"required,min=3"`
 	LastName  string `validate:"required,min=3"`
 	Username  string `validate:"required,username,min=3,max=32" gorm:"unique"`
-	Email     string `validate:"required,email" gorm:"unique"`
+	Email     string `validate:"required,email"                 gorm:"unique"`
 	Password  string `validate:"required,password,min=12,max=48"`
 
 	// TODO: Implement rest of the fields
 }
 
 type UserRegister struct {
-	FirstName string `validate:"required,min=3" json:"firstname"`
-	LastName  string `validate:"required,min=3" json:"lastname"`
-	Username  string `validate:"required,username,min=3,max=32" json:"username" gorm:"unique"`
-	Email     string `validate:"required,email" json:"email" gorm:"unique"`
+	FirstName string `validate:"required,min=3"                  json:"firstname"`
+	LastName  string `validate:"required,min=3"                  json:"lastname"`
+	Username  string `validate:"required,username,min=3,max=32"  json:"username" gorm:"unique"`
+	Email     string `validate:"required,email"                  json:"email"    gorm:"unique"`
 	Password  string `validate:"required,password,min=12,max=48" json:"password"`
 }

0d6d364216529f2cf8926e8e9ec1d38cb2e8db6e
devkcud <patommmmm@proton.me>
2024-05-04 14:47:53 -0300

    Remove types and add initial mocking

--- internal/service/repository/types.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-type Repository[T any] interface {
-	Store(*T) error
-	Update(uint, *T) error
-	Find(*T) (*T, error)
-	Delete(uint) error
-}
--- internal/service/repository/user.go
+++ internal/service/repository/user.go
@@ -1,11 +1,6 @@
 package repository
 
 import (
-	"fmt"
-	"reflect"
-	"strings"
-	"sync"
-
 	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/model"
 	"github.com/devkcud/arkhon-foundation/arkhon-api/pkg/db"
 	"gorm.io/gorm"
@@ -15,10 +10,11 @@ type userRepository struct {
 	db *gorm.DB
 }
 
-// NOTE: As we modify the Repo default methods, we need to create a new repository (which imo is better for readability)
-// Now, we should continue this pattern. Create a repository for every model :) (models that overlap, e.g. user and comments, can be merged together)
 type UserRepository interface {
-	Repository[model.User]
+	Store(*model.User) error
+	Update(uint, *model.User) error
+	Find(*model.User) (*model.User, error)
+	Delete(uint) error
 }
 
 func NewUserRepository() UserRepository {
@@ -34,41 +30,9 @@ func (u userRepository) Update(id uint, updateModel *model.User) error {
 }
 
 func (u userRepository) Find(searchModel *model.User) (*model.User, error) {
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-
-	fields := reflect.TypeOf(*searchModel)
-	values := reflect.ValueOf(*searchModel)
-
-	var conditions []string
-	var queryValues []any
-
-	for i := 0; i < fields.NumField(); i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-
-			value := values.Field(i)
-
-			if value.IsZero() {
-				return
-			}
-
-			// FIXME: Hardcoded() "users" table name, not ideal for when the name change in the future
-			fieldName := u.db.NamingStrategy.ColumnName("users", fields.Field(i).Name)
-
-			mu.Lock()
-			conditions = append(conditions, fmt.Sprintf("%s = ?", fieldName))
-			queryValues = append(queryValues, value.Interface())
-			mu.Unlock()
-		}(i)
-	}
-
-	wg.Wait()
-
 	var user model.User
 
-	if err := u.db.Where(strings.Join(conditions, " OR "), queryValues...).First(&user).Error; err != nil {
+	if err := u.db.First(&user, searchModel).Error; err != nil {
 		return nil, err
 	}
 

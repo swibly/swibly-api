58f5cf36fcc532e684cb30a41e2e995f1c411706
devkcud <patommmmm@proton.me>
2024-05-15 10:35:52 -0300

    Add update profile

--- internal/controller/http/v1/profile.go
+++ internal/controller/http/v1/profile.go
@@ -1,10 +1,15 @@
 package v1
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/model"
 	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/service/usecase"
+	"github.com/devkcud/arkhon-foundation/arkhon-api/pkg/middleware"
+	"github.com/devkcud/arkhon-foundation/arkhon-api/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -13,6 +18,7 @@ func newProfileRoutes(handler *gin.RouterGroup) {
 	h := handler.Group("/profile")
 	{
 		h.GET("/:username", GetProfileHandler)
+		h.PATCH("/:idlookup", middleware.AuthMiddleware, UpdateProfileHandler)
 	}
 }
 
@@ -32,7 +38,7 @@ func GetProfileHandler(ctx *gin.Context) {
 			"createdat": user.CreatedAt,
 			"updatedat": user.UpdatedAt,
 
-			"roles": user.Roles,
+			"role": user.Role,
 
 			"firstname": user.FirstName,
 			"lastname":  user.LastName,
@@ -60,4 +66,100 @@ func GetProfileHandler(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error. Please, try again later."})
+}
+
+func UpdateProfileHandler(ctx *gin.Context) {
+	// We know it exists, no need to pass in exists variable
+	usecaseInterface, _ := ctx.Get("uc")
+	// We know it will always be a UserUseCase
+	usecase, _ := usecaseInterface.(usecase.UserUseCase)
+
+	uid, _ := ctx.Get("userid")
+	issuerID, _ := uid.(uint)
+
+	lid := ctx.Param("idlookup")
+	lookupID, err := strconv.Atoi(lid)
+
+	if err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad ID"})
+		return
+	}
+
+	issuer, err := usecase.GetByID(issuerID)
+
+	if err != nil {
+		log.Print(err)
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "No user found with that ID"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error. Please, try again later."})
+		return
+	}
+
+	lookup, err := usecase.GetByID(uint(lookupID))
+
+	if err != nil {
+		log.Print(err)
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "No user found with that ID"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error. Please, try again later."})
+		return
+	}
+
+	var body model.ProfileUpdate
+
+	if err := ctx.BindJSON(&body); err != nil {
+		log.Print(err)
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Bad body format"})
+		return
+	}
+
+	if errs := utils.ValidateStruct(body); errs != nil {
+		err := utils.ValidateErrorMessage(errs[0])
+
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": gin.H{err.Param: err.Message}})
+		return
+	}
+
+	if issuer.ID == lookup.ID {
+		var newModel model.User
+
+		newModel.FirstName = body.FirstName
+		newModel.LastName = body.LastName
+		newModel.Username = body.Username
+		newModel.Email = body.Email
+
+		newModel.Show.Profile = body.Show.Profile
+		newModel.Show.Image = body.Show.Image
+		newModel.Show.Comments = body.Show.Comments
+		newModel.Show.Favorites = body.Show.Favorites
+		newModel.Show.Projects = body.Show.Projects
+		newModel.Show.Components = body.Show.Components
+		newModel.Show.Followers = body.Show.Followers
+		newModel.Show.Following = body.Show.Following
+		newModel.Show.Inventory = body.Show.Inventory
+		newModel.Show.Formations = body.Show.Formations
+
+		newModel.Notification.InApp = body.Notification.InApp
+		newModel.Notification.Email = body.Notification.Email
+
+		if err := usecase.Update(lookup.ID, &newModel); err != nil {
+			log.Print(err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error. Please, try again later."})
+		}
+		return
+	}
+
+	if issuer.Role != "admin" {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "You cannot update another user"})
+		return
+	}
 }
--- internal/model/user.go
+++ internal/model/user.go
@@ -6,6 +6,11 @@ import (
 	"gorm.io/gorm"
 )
 
+var (
+	ROLE_ADMIN = "admin"
+	ROLE_USER  = "user"
+)
+
 type User struct {
 	// NOTE: Not using gorm.Model since it's properties cannot be accessed directly
 	ID        uint `gorm:"primarykey"`
@@ -13,7 +18,7 @@ type User struct {
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 
-	Roles []Role `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;many2many:user_roles;"`
+	Role string `gorm:"default:user"`
 
 	FirstName string
 	LastName  string
@@ -31,22 +36,22 @@ type User struct {
 	// TODO: Add comments, the last implementation was wacky to say the least.
 
 	Notification struct {
-		InApp bool `gorm:"default:true"`
-		Email bool `gorm:"default:false"`
-		// SMS bool `gorm:"default:false"` // NOTE: Not sure if we want to send SMS, it can get expensive
+		InApp int `gorm:"default:1"  json:"inapp"`
+		Email int `gorm:"default:-1" json:"email"`
+		// SMS int `gorm:"default:-1"` // NOTE: Not sure if we want to send SMS, it can get expensive
 	} `gorm:"embedded;embeddedPrefix:notify_"`
 
 	Show struct {
-		Profile    bool `gorm:"default:true"`
-		Image      bool `gorm:"default:true"`
-		Comments   bool `gorm:"default:true"`
-		Favorites  bool `gorm:"default:true"`
-		Projects   bool `gorm:"default:true"`
-		Components bool `gorm:"default:true"`
-		Followers  bool `gorm:"default:true"`
-		Following  bool `gorm:"default:true"`
-		Inventory  bool `gorm:"default:false"`
-		Formations bool `gorm:"default:true"`
+		Profile    int `gorm:"default:1" json:"profile"`
+		Image      int `gorm:"default:1" json:"image"`
+		Comments   int `gorm:"default:1" json:"comments"`
+		Favorites  int `gorm:"default:1" json:"favorites"`
+		Projects   int `gorm:"default:1" json:"projects"`
+		Components int `gorm:"default:1" json:"components"`
+		Followers  int `gorm:"default:1" json:"followers"`
+		Following  int `gorm:"default:1" json:"following"`
+		Inventory  int `gorm:"default:-1" json:"inventory"`
+		Formations int `gorm:"default:1" json:"formations"`
 	} `gorm:"embedded;embeddedPrefix:show_"`
 
 	// TODO: Implement enums Language, Theme and Country (country shouldnt be an enum)
@@ -64,4 +69,28 @@ type UserLogin struct {
 	Username string `validate:"omitempty,username,min=3,max=32" json:"username"`
 	Email    string `validate:"omitempty,email"                 json:"email"`
 	Password string `validate:"required,password,min=12,max=48" json:"password"`
+}
+
+type ProfileUpdate struct {
+	FirstName string `validate:"omitempty,min=3"                 json:"firstname"`
+	LastName  string `validate:"omitempty,min=3"                 json:"lastname"`
+	Username  string `validate:"omitempty,username,min=3,max=32" json:"username"`
+	Email     string `validate:"omitempty,email"                 json:"email"`
+	Bio       string `validate:"omitempty"                       json:"bio"`
+	Show      struct {
+		Profile    int `json:"profile"`
+		Image      int `json:"image"`
+		Comments   int `json:"comments"`
+		Favorites  int `json:"favorites"`
+		Projects   int `json:"projects"`
+		Components int `json:"components"`
+		Followers  int `json:"followers"`
+		Following  int `json:"following"`
+		Inventory  int `json:"inventory"`
+		Formations int `json:"formations"`
+	} `json:"show"`
+	Notification struct {
+		InApp int `json:"inapp"`
+		Email int `json:"email"`
+  } `json:"notification"`
 }

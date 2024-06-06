2d1c90b2b20fa418ed2a47425052c9b6a44041c8
devkcud <patommmmm@proton.me>
2024-06-06 10:12:30 -0300

    Add permissions configuration

--- config/config.go
+++ config/config.go
@@ -28,6 +28,14 @@ var (
 		BcryptCost int    `yaml:"bcrypt_cost"`
 		JWTSecret  string `yaml:"jwt_secret"`
 	}
+
+	Permissions struct {
+		Admin             string `yaml:"admin"`
+		ManageUser        string `yaml:"manage_user"`
+		ManagePermissions string `yaml:"manager_permissions"`
+		ManageProjects    string `yaml:"manage_projects"`
+		ManageStore       string `yaml:"manage_store"`
+	}
 )
 
 func Parse() {
@@ -44,6 +52,10 @@ func Parse() {
 	}
 
 	if err := yaml.Unmarshal(read("security.yaml"), &Security); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	if err := yaml.Unmarshal(read("permissions.yaml"), &Permissions); err != nil {
 		log.Fatalf("error: %v", err)
 	}
 

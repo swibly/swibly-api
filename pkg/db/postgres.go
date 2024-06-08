302303ea8f47db9bb2befdb79cf31659c962e590
devkcud <patommmmm@proton.me>
2024-06-08 00:35:08 -0300

    Create type enum_language

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -3,15 +3,34 @@ package db
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/devkcud/arkhon-foundation/arkhon-api/config"
 	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/model"
+	"github.com/devkcud/arkhon-foundation/arkhon-api/pkg/language"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 var Postgres *gorm.DB
+
+func typeCheckAndCreate(db *gorm.DB, typeName string, values []string) error {
+	var typeExists bool
+	err := db.Raw("SELECT EXISTS (SELECT 1 FROM pg_type WHERE typname =?)", typeName).Scan(&typeExists).Error
+	if err != nil {
+		return fmt.Errorf("error checking type existence: %w", err)
+	}
+
+	if !typeExists {
+		err := db.Exec("CREATE TYPE %s AS ENUM (%s)", typeName, strings.Join(values, ", ")).Error
+		if err != nil {
+			return fmt.Errorf("error creating type: %w", err)
+		}
+	}
+
+	return nil
+}
 
 func Load() {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=%s", config.Postgres.Host, config.Postgres.User, config.Postgres.Password, config.Postgres.DB, config.Postgres.SSLMode)
@@ -26,6 +45,12 @@ func Load() {
 	Postgres = db
 
 	log.Print("Loaded Database")
+
+	if err := typeCheckAndCreate(Postgres, "enum_language", language.ArrayString); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Loaded migrations")
 
 	if err := Postgres.AutoMigrate(
 		&model.User{},
@@ -43,6 +68,4 @@ func Load() {
 		{Name: config.Permissions.ManageProjects},
 		{Name: config.Permissions.ManageStore},
 	})
-
-	log.Print("Loaded migrations")
 }

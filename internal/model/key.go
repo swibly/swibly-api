4b34723ca50aa20fb788964fe061c9cc75ea54cb
André devkcud Albanese Junior <patommmmm@proton.me>
2024-07-04 15:44:08 -0300

    Add API key model and load migrations

---
+++ internal/model/key.go
@@ -0,0 +1,11 @@
+package model
+
+type APIKey struct {
+	Key string `gorm:"primarykey"`
+
+	EnabledKeyGen      bool `gorm:"default:false"` // Generate new API keys
+	EnabledAuth        bool `gorm:"default:false"` // Register, Login, Update, Delete
+	EnabledSearch      bool `gorm:"default:false"` // User
+	EnabledUserFetch   bool `gorm:"default:false"` // Profile, Permissions, Followers, Following
+	EnabledUserActions bool `gorm:"default:false"` // Follow, Unfollow
+}

cc759bcdccae980ff60b267ca9f5b3bf702f88fb
André devkcud Albanese Junior <patommmmm@proton.me>
2024-08-09 15:36:55 -0300

    fix: Remove checks for fields

--- internal/model/dto/user.go
+++ internal/model/dto/user.go
@@ -3,30 +3,30 @@ package dto
 import "github.com/devkcud/arkhon-foundation/arkhon-api/pkg/language"
 
 type UserRegister struct {
-	FirstName string `validate:"required,min=3"                  json:"firstname"`
-	LastName  string `validate:"required,min=3"                  json:"lastname"`
-	Username  string `validate:"required,username,min=3,max=32"  json:"username"`
-	Email     string `validate:"required,email"                  json:"email"`
+	FirstName string `validate:"required,min=3"    json:"firstname"`
+	LastName  string `validate:"required,min=3"    json:"lastname"`
+	Username  string `validate:"required,username" json:"username"`
+	Email     string `validate:"required,email"    json:"email"`
 	Password  string `validate:"required,password" json:"password"`
 }
 
 type UserLogin struct {
-	Username string `validate:"omitempty,username,min=3,max=32" json:"username"`
-	Email    string `validate:"omitempty,email"                 json:"email"`
-	Password string `validate:"required,password" json:"password"`
+	Username string `validate:"omitempty" json:"username"`
+	Email    string `validate:"omitempty" json:"email"`
+	Password string `validate:"required"  json:"password"`
 }
 
 type UserUpdate struct {
-	FirstName string `validate:"omitempty,min=3"                 json:"firstname"`
-	LastName  string `validate:"omitempty,min=3"                 json:"lastname"`
-	Username  string `validate:"omitempty,username,min=3,max=32" json:"username"`
+	FirstName string `validate:"omitempty,min=3"    json:"firstname"`
+	LastName  string `validate:"omitempty,min=3"    json:"lastname"`
+	Username  string `validate:"omitempty,username" json:"username"`
 
 	Bio      string `validate:"omitempty,max=480" json:"bio"`
 	Verified bool   `validate:"omitempty"         json:"verified"`
 
 	// NOTE: XP and Arkhoins doesn't make sense to update here
 
-	Email    string `validate:"omitempty,email"                  json:"email"`
+	Email    string `validate:"omitempty,email"    json:"email"`
 	Password string `validate:"omitempty,password" json:"password"`
 
 	Notification struct {

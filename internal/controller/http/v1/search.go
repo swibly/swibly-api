20cbda44e42b5376f0c1ee49a0846ed7206268bd
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-06 15:37:05 -0300

    feat: Add search project

--- internal/controller/http/v1/search.go
+++ internal/controller/http/v1/search.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/model/dto"
 	"github.com/devkcud/arkhon-foundation/arkhon-api/internal/service"
 	"github.com/devkcud/arkhon-foundation/arkhon-api/pkg/middleware"
 	"github.com/devkcud/arkhon-foundation/arkhon-api/translations"
@@ -17,9 +18,10 @@ import (
 
 func newSearchRoutes(handler *gin.RouterGroup) {
 	h := handler.Group("/search")
-	h.Use(middleware.APIKeyHasEnabledSearch)
+	h.Use(middleware.APIKeyHasEnabledSearch, middleware.Auth)
 	{
 		h.GET("/user", SearchUserByNameHandler)
+		h.GET("/project", SearchProjectByNameHandler)
 	}
 }
 
@@ -59,4 +61,44 @@ func SearchUserByNameHandler(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, users)
+}
+
+func SearchProjectByNameHandler(ctx *gin.Context) {
+	dict := translations.GetTranslation(ctx)
+
+	name := ctx.Query("name")
+
+	if !regexp.MustCompile(`[a-zA-Z ]`).MatchString(name) || strings.TrimSpace(name) == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": dict.SearchIncorrect})
+		return
+	}
+
+	issuer := ctx.Keys["auth_user"].(*dto.UserProfile)
+
+	var (
+		page    int = 1
+		perpage int = 10
+	)
+
+	if i, e := strconv.Atoi(ctx.Query("page")); e == nil && ctx.Query("page") != "" {
+		page = i
+	}
+
+	if i, e := strconv.Atoi(ctx.Query("perpage")); e == nil && ctx.Query("perpage") != "" {
+		perpage = i
+	}
+
+	projects, err := service.Project.SearchByName(issuer.ID, name, page, perpage)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": dict.SearchNoResults})
+			return
+		}
+
+		log.Print(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": dict.InternalServerError})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, projects)
 }

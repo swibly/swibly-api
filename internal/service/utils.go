77fb7a638b67f512d5b13f723cc98c774233e9f6
André devkcud Albanese <patommmmm@proton.me>
2024-11-04 07:28:24 -0300

    chore: Remove unused ctx param from `CreateNotification`

--- internal/service/utils.go
+++ internal/service/utils.go
@@ -1,11 +1,8 @@
 package service
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/swibly/swibly-api/internal/model/dto"
-)
+import "github.com/swibly/swibly-api/internal/model/dto"
 
-func CreateNotification(ctx *gin.Context, createModel dto.CreateNotification, ids ...uint) error {
+func CreateNotification(createModel dto.CreateNotification, ids ...uint) error {
 	notification, err := Notification.Create(createModel)
 	if err != nil {
 		return err
